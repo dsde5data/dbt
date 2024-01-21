e5e674daf68cb7f9467d46e9191b4c007df39046
Ali  Shahbaz <asameen307@gmail.com>
2024-01-21 16:31:17 +0000

    adding tests

--- models/staging/orders.sql
+++ models/staging/orders.sql
@@ -1,4 +1,4 @@
 with cte_ORDERS as (
-select ID as ORDER_ID,user_id as customer_id,ORDER_DATE,status ORDER_STATUS from {{ source('jaffle_shop', 'orders') }}
+select ID as ORDER_ID,user_id as customer_id,ORDER_DATE,status ORDER_STATUS,_etl_loaded_at from {{ source('jaffle_shop', 'orders') }}
 )
 select * from cte_ORDERS
