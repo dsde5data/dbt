50bf37ad0f73971341fd5f06e78f2a4ee1ffc1a2
Ali  Shahbaz <asameen307@gmail.com>
2024-01-22 06:44:18 +0000

    fixing customer_facts in bi layer

--- models/marts/customer_orders.sql
+++ models/marts/customer_orders.sql
@@ -4,9 +4,9 @@
 with final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        a.customer_id,
+        a.first_name,
+        a.last_name,
         c.first_order_date,
         c.most_recent_order_date,
         coalesce(c.number_of_orders, 0) as number_of_orders
